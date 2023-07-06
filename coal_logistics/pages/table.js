373aae50eb14ff6cbb3fc0cfa5c495906e96cf5e
HyperDanirio <danieru1115@proton.me>
2023-07-06 09:29:14 +0800

    Daniel_Push_07/06/23/1

--- coal_logistics/pages/table.js
+++ coal_logistics/pages/table.js
@@ -24,7 +24,7 @@ export default function table() {
 
   return (
     <Layout>
-      <Sider>
+      <Sider trigger={null} collapsible collapsed={collapsed} className="px-4">
         <div
           className={
             collapsed
@@ -41,17 +41,75 @@ export default function table() {
           defaultSelectedKeys={["1"]}
         >
           <Menu.Item>
-            <Link className={collapsed ? "hidden" : ""} href="/" key={1}>
+            <Link
+              className={
+                collapsed ? "hidden" : "flex justify-start items-center text-xs"
+              }
+              href="/"
+              key={1}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#D3D3D3"
+                class="w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              <div className="w-2"></div>
               Хяналтын Самбар
             </Link>
           </Menu.Item>
           <Menu.Item>
-            <Link className={collapsed ? "hidden" : ""} href="/table" key={2}>
+            <Link
+              className={
+                collapsed ? "hidden" : "flex justify-start items-center text-xs"
+              }
+              href="/table"
+              key={2}
+            >
+              <svg
+                fill="none"
+                stroke="#D3D3D3"
+                strokeWidth="1"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                class="w-3.5 h-3.5  "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+                />
+              </svg>
+              <div className="w-2"></div>
               Хүснэгт
             </Link>
           </Menu.Item>
           <Menu.Item>
-            <Link className={collapsed ? "hidden" : ""} href="/ovoolgo" key={3}>
+            <Link
+              className={
+                collapsed ? "hidden" : "flex justify-start items-center text-xs"
+              }
+              href="/ovoolgo"
+              key={3}
+            >
+              <div class="w-3.5 h-3.5 flex justify-center items-center">
+                <svg class="w-full h-full flex justify-center items-center">
+                  <polygon
+                    points="6,2 0,12 12,12"
+                    class=" stroke-[#D3D3D3] stroke-1.5 fill-none"
+                  />
+                </svg>
+              </div>
+              <div className="w-2"></div>
               Овоолго
             </Link>
           </Menu.Item>
